21711f567936a0df56f9617872df292d5a2a888d
Domingos Ribeiro <domingos.ribeiro@gmail.com>
2021-08-29 15:50:50 +0100

    UnitOfWork

---
+++ UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebApplication1.DAL;
+using WebApplication1.Models;
+using WebApplication1.Repository;
+
+
+namespace WebApplication1.UnitOfWork
+{
+    public class UnitOfWork : DbContext
+    {
+        Context context = new Context();
+        Repository<Products> productsRepository;
+
+        public Repository<Products> ProductsRepository
+        {
+            get
+            {
+                if(ProductsRepository == null)
+                {
+                    productsRepository = new Repository<Products>(context);
+                }
+                return productsRepository;
+            }
+        }
+    }
+}
