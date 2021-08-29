c9736343acce2f4e0aa8b87dbc7beef95390e1c7
Domingos Ribeiro <domingos.ribeiro@gmail.com>
2021-08-29 13:56:32 +0100

    crud

--- Repository/Repository.cs
+++ Repository/Repository.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Web;
 using WebApplication1.DAL;
 
@@ -25,6 +26,35 @@ namespace WebApplication1.Repository
         public void Commit()
         {
             Context.SaveChanges();
+        }
+        public void Edit(T entity)
+        {
+            Context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public IQueryable<T> FindAll()
+        {
+            return Context.Set<T>();
+        }
+
+        public IQueryable<T> FindBy(Expression<Func<T, bool>> predicate)
+        {
+            return DbSet.Where(predicate);
+        }
+
+        public T FindById(int Id)
+        {
+            return DbSet.Find(Id);
+        }
+
+        public void Remove(T entity)
+        {
+            Context.Set<T>().Remove(entity);
+        }
+
+        public void Add(T entity)
+        {
+            Context.Set<T>().Add(entity);
         }
     }
 }
