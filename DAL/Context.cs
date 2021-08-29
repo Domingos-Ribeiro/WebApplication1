951dec67876c711fbfb468532f34e9083cdf7071
Domingos Ribeiro <domingos.ribeiro@gmail.com>
2021-08-29 13:26:56 +0100

    Add project files.

---
+++ DAL/Context.cs
@@ -0,0 +1,33 @@
+﻿using WebApplication1;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace WebApplication1.DAL
+{
+    public class Context : DbContext
+    {
+        public Context() : base("DefaultConnection")
+
+        {
+            
+        }
+
+        public virtual  DbSet<Models.Products> Product { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasColumnType("varchar"));
+
+            modelBuilder.Properties()
+                .Where(p => p.Name == "Id")
+                .Configure(p => p.IsKey());
+        }
+    }
+}
