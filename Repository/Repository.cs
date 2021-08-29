1ab60dc538a129111a4df08a628f192000454358
Domingos Ribeiro <domingos.ribeiro@gmail.com>
2021-08-29 13:38:39 +0100

    Repository

---
+++ Repository/Repository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebApplication1.DAL;
+
+namespace WebApplication1.Repository
+{
+    public class Repository<T> : IDisposable where T : class
+    {
+        Context Context;
+        DbSet<T> DbSet;
+        public Repository(Context context)
+        {
+            Context = context;
+            DbSet = context.Set<T>();
+        }
+
+
+        public void Dispose()
+        {
+            Context.Dispose();
+        }
+        public void Commit()
+        {
+            Context.SaveChanges();
+        }
+    }
+}
